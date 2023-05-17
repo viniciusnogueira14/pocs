65d691fc92ae044aa9423d9450831633e1046083
Vinicius Figueiredo Nogueira <comp.vinicius@gmail.com>
2023-05-17 15:13:17 -0300

    Initial commit

---
+++ src/test/java/com/dev/nicknogueira/pocs/PoCsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.nicknogueira.pocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoCsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
